f62aee517f4e6a72e4acc2926b3bdb79b911c008
btow <btow@yandex.ru>
2016-11-30 17:20:31 +0500

    The class Frontend has ben created.

---
+++ src/main/java/Frontend.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * The class contained a servlet ""
+ * Created by btow on 30.11.2016.
+ */
+public class Frontend extends HttpServlet {
+
+    private String login = " ";
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws ServletException, IOException {
+
+    }
+
+    public void doPost(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+}
